bddfce10f73ba7c39cb5e83a73af4f38f4afdf53
Revanites2113 <joro2073@colorado.edu>
2022-10-11 13:26:09 -0600

    Player Movement and Animation
    Just like mario64, these boss fights need good player movement so this is first

---
+++ Dungeon Boss Run/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //this script will also handle transitions between animation states (still all just movement related)
+    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private Animator movementAni;
+    private Vector3 _moveDir;
+    private int _thrust = 10;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        int moveX = 0;
+        int moveY = 0;
+        
+        //resetting animation state every frame so not stuck in states
+        movementAni.SetBool("isMoving", false);
+        movementAni.SetBool("isRight", false);
+        movementAni.SetBool("isLeft", false);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1;
+            movementAni.SetBool("isRight", true);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1;
+            movementAni.SetBool("isLeft", true);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1;
+            movementAni.SetBool("isRight", true);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1;
+            movementAni.SetBool("isLeft", true);
+        }
+
+        _moveDir = new Vector3(moveX, moveY, 0);
+        playerRb.MovePosition(transform.position + (_moveDir * Time.deltaTime * _thrust));
+
+        if (_moveDir != Vector3.zero)
+        {
+            movementAni.SetBool("isMoving", true);
+        }
+    }
+}
